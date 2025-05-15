5460e25877d11312032fd17134da994acaeecf39
Hikari Hashiguchi <its.hikarih@gmail.com>
2025-05-15 16:14:03 +0900

    complete models

---
+++ app/models/bookmark.rb
@@ -0,0 +1,11 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  # validates :movie, presence: true
+  # validates :list, presence: true
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, presence: true
+  validates :list, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id }
+end
