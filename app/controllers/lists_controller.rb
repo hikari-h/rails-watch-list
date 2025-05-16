9b7ca2e5ffe2abb0d9ad01a0239d307fb9eef54d
Hikari Hashiguchi <its.hikarih@gmail.com>
2025-05-16 12:15:06 +0900

    added code to watch list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      @lists = List.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
